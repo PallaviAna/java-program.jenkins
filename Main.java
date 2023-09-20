2cd846dd63b1f2a5a3899b8018943534c1488970
Pallavi <pallavi.anasune07@gmail.com>
2023-09-20 11:52:53 +0530

    Added new data 1

--- Main.java
+++ Main.java
@@ -6,5 +6,8 @@ public class Main{
         System.out.println("output from Java program");
         System.out.println(new Date());
         System.out.println("Added 1 more file");
+
+        Student student=new Student();
+        student.sayHello();
     }
 }
