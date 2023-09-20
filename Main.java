e23bee0f523d4c14b89e3c47e850f63ffadbe036
Pallavi <pallavi.anasune07@gmail.com>
2023-09-20 11:23:41 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("This is Java program");
+        System.out.println("output from Java program");
+        System.out.println(new Date());
+    }
+}
